70890320582bb6d79a0fd4982b21aa7d9465326c
caitongy <yehcaitong@gmail.com>
2018-12-28 21:55:45 +0100

    Add data1, data2, plotData

--- machine_learning_python/W1/ex1.py
+++ machine_learning_python/W1/ex1.py
@@ -1,8 +1,8 @@
 ## Machine Learning Online Class - Exercise 1: Linear Regression
-
-# Instructions
+## in python 3.7
+# Functions implemented
 #  ------------
-#
+#  warmUpExercise.py
 #
 #
 # x refers to the population size in 10,000s
@@ -13,20 +13,32 @@
 import os
 import sys
 import numpy as np
+from matplotlib import pyplot as plt
 
 from warmUpExercise import warmUpExercise
-
+from plotData import plotData
 
 def pause():
     programPause = input('Program paused. Press enter to continue.')
 
 
 ## ==================== Part 1: Basic Function ====================
-# Complete warmUpExercise.m
+# refer to warmUpExercise.py
 print('Running warmUpExercise ... ')
 Matrix = warmUpExercise()
 
 print('5x5 Identity Matrix: ')
 print(Matrix)
 
+pause()
+
+## ======================= Part 2: Plotting =======================
+print('Plotting Data ...')
+data = np.loadtxt('ex1data1.txt', delimiter = ',');
+X = data[:, 0]; y = data[:, 1];
+m = len(y); # number of training examples
+
+# Plot Data
+# Note: refer to plotData.py
+plotData(X,y)
 pause()
