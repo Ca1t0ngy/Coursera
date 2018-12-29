f2f7569f8e354356d688670a323c31bcf44bee11
caitongy <yehcaitong@gmail.com>
2018-12-29 15:14:00 +0100

    add computeCost

---
+++ machine_learning_python/W1/computeCost.py
@@ -0,0 +1,16 @@
+def computeCost(X, y, theta):
+    # COMPUTECOST Compute cost for linear regression
+    #   J = COMPUTECOST(X, y, theta) computes the cost of using theta as the
+    #   parameter for linear regression to fit the data points in X and y
+
+    # Initialize some useful values
+    m = len(y); # number of training examples
+
+    #  need to return the following variables correctly
+    J = 0;
+    for i in range(m):
+        J = J + (theta[0, 0]+ theta[1, 0]* X[i, 1] - y[i])**2;
+
+    J = J/(2*m);
+
+    return J
--- machine_learning_python/W1/ex1.py
+++ machine_learning_python/W1/ex1.py
@@ -17,6 +17,7 @@ from matplotlib import pyplot as plt
 
 from warmUpExercise import warmUpExercise
 from plotData import plotData
+from computeCost import computeCost
 
 def pause():
     programPause = input('Program paused. Press enter to continue.')
@@ -37,8 +38,62 @@ print('Plotting Data ...')
 data = np.loadtxt('ex1data1.txt', delimiter = ',');
 X = data[:, 0]; y = data[:, 1];
 m = len(y); # number of training examples
-
 # Plot Data
 # Note: refer to plotData.py
 plotData(X,y)
 pause()
+
+## =================== Part 3: Cost and Gradient descent ===================
+X = np.zeros((m, 2))
+X[:, 0] = np.ones(m) # Add a column of ones to x
+X[:, 1] = data[:, 0]
+
+theta = np.zeros((2, 1)); # initialize fitting parameters
+
+# Some gradient descent settings
+iterations = 1500;
+alpha = 0.01;
+
+print('Testing the cost function ...')
+# compute and display initial cost; refer to computeCost
+J = computeCost(X, y, theta);
+print('With theta = [0 ; 0]\nCost computed = {0:.2f}'.format(J));
+print('Expected cost value (approx) 32.07');
+
+pause();
+# further testing of the cost function
+theta = np.array([[-1], [2]])
+J = computeCost(X, y, theta);
+print('With theta = [-1 ; 2]\nCost computed = {0:.2f}'.format(J));
+print('Expected cost value (approx) 54.24');
+
+print('Program paused. Press enter to continue.');
+
+"""
+print('Running Gradient Descent ...')
+% run gradient descent
+theta = gradientDescent(X, y, theta, alpha, iterations);
+
+% print theta to screen
+fprintf('Theta found by gradient descent:\n');
+fprintf('%f\n', theta);
+fprintf('Expected theta values (approx)\n');
+fprintf(' -3.6303\n  1.1664\n\n');
+
+% Plot the linear fit
+hold on; % keep previous plot visible
+plot(X(:,2), X*theta, '-')
+legend('Training data', 'Linear regression')
+hold off % don't overlay any more plots on this figure
+
+% Predict values for population sizes of 35,000 and 70,000
+predict1 = [1, 3.5] *theta;
+fprintf('For population = 35,000, we predict a profit of %f\n',...
+    predict1*10000);
+predict2 = [1, 7] * theta;
+fprintf('For population = 70,000, we predict a profit of %f\n',...
+    predict2*10000);
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
+"""
